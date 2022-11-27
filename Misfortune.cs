0347234ef98c1ba16a99c8ed88c42e057b93b095
Kalle <ketola.kalle@gmail.com>
2022-11-27 12:05:08 +0200

    Added texts.

--- Misfortune.cs
+++ Misfortune.cs
@@ -7,28 +7,27 @@
 
         readonly List<string> MisfortunesFinPart1 = new()
         {
-        "Tapaat värittömän ", "Kohtaat mysteerisen ", "Ymmärrät ", "Näet unessa ", "Tunnet kosmisen "
+        "Kohtaat värittömän ", "Kohtaat mysteerisen ", "Kohtaat vesimelooneilta haisevan ", "Ymmärrät ", "Näet unessa ", "Tunnet kosmisen, ", "Ennustajan hämärässä huoneessa tunnet ", "Haistat ", "Eksyneenä metsään kohtaat ", "Marjastamassa kohtaat "
         };
         readonly List<string> MisfortunesFinPart2 = new()
         {
-         "henkilön ", "kuusi sorminen lapsen ", "varjon pimeässä ", "paaviksi pukeutuneen nuoren naisen"
+         "henkilön ", "kuusi sorminen lapsen ", "varjon pimeässä ", "paaviksi pukeutuneen nuoren naisen ", "puolison menneestä elämästäsi ", "lapsesi tulevaisuudesta ", "haisevan alkoholistin, jonka housuihin on ommeltu nimikirjaimet JFK ", "eksyneen marjastajan", 
+            "kauan sitten kuolleen tunnnetun jazz-muusikon "
         };
         readonly List<string> MisfortunesFinPart3 = new()
         {
-         " joka kuiskaa ", " jonka tatuointi alaselässä kertoo ", "joka ojentaa sinulle "
+         "joka kuiskaa paikan josta löydät ", "jonka tatuointi alaselässä kertoo sijainnnin josta löydät ", "joka ojentaa sinulle ", "joka myöhemmin lähettää kirjeen kotiisi joka sisältää ", "ottaa taskustaan pienen tuntemattoman instrumentit jolla soittaa "
         };
         readonly List<string> MisfortunesFinPart4 = new()
         {
-            "totuuden ", "valheen ", "tärkeän tiedon ", "epämääräistä tekstiä", "haalistuneen lottotositteen vuosien takaa "
+            "totuuden ", "valheen ", "tärkeän tiedon ", "epämääräistä tekstiä", "haalistuneen lottotositteen vuosien takaa, jonka taakse on lyijykynällä piirretty kuva kissasta ", "lyhyen mutta kauniin melodian "
         };
         readonly List<string> MisfortunesFinPart5 = new()
         {
-            ", älä unohda sitä.", ", kirjoita se postikorttiin jonka lähetät etäiselle sukulaisellesi.", ", se ohjaa sinua matkallasi.", ", olet nyt varma ettemme ole yksin universumissa"
+            ", älä unohda sitä.", ", kirjoita se postikorttiin jonka lähetät etäiselle sukulaisellesi.", ", se ohjaa sinua matkallasi.", ", olet nyt varma ettemme ole yksin universumissa.", "syystä jota et ymmärrä vietät seuraavan viikon itkien sängyssäsi.",
+            "muista se kun sinulta kysytään viimeisiä sanojasi.", "tämä tietoo saattaa sinut pelastaa.", ", jos eksyt unessasi metsään vain tämä tieto saattaa johdattaa sinut sieltä pois.", "kuiskaa se lapsenlapsesi korvaan vuosien päästä."
         };
-        readonly List<string> MisfortunesChineseP1 = new()
-        { 
-            "fdlkslkfdlk"
-        };
+
 
         public void GetMisfortune()
         {
@@ -49,9 +48,9 @@
             int RandomNumber5 = rnd.Next(MisfortunesFinPart5.Count);
             string FinMisFortune5 = MisfortunesFinPart5[RandomNumber5];
 
-            //string ChineseMisFortune = MisfortunesChinese[RandomNumber];
+           
             this.TextFin = FinMisFortune + FinMisFortune2 + FinMisFortune3 + FinMisFortune4 + FinMisFortune5;
-            //this.TextChinese = ChineseMisFortune;
+            
             return;
         }
     }
