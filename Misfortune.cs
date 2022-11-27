26eb65996e95ddea4fa17f347fcb75582a7f99b3
Kalle <ketola.kalle@gmail.com>
2022-11-27 10:33:39 +0200

    Base for side branch.

--- Misfortune.cs
+++ Misfortune.cs
@@ -6,24 +6,12 @@
         public string TextChinese { get; set; }
 
         readonly List<string> MisfortunesFin = new()
-        {"Soittamalla Metallican kill em all -levyä keskellä yötä saa naapurisi tuomaan sinulle epäonnea aamulla.",
-         "Pelaamasi rivit lottoa johtavat rahan menetykseen.",
-         "Kissanruoka loppuu, josta suuttuneena kissa raapii pilalle tapettisi",
-         "Olet vuosia tervehtinyt Naapuriasi nimellä Pasi. Tänään sinulle selviää, että Pasin oikea nimi onkin Sami.",
-         "Astut legoon olohuoneessa. Omituiseksi asian tekee se ettei sinulla ole lapsia ja viimeksi olet leikkinyt tai omistanut legoja kymmeniä vuosia sitten. Et saa tätä asiaa mielestäsi viikkoihin.",
-         "Ostat väärää ketsuppia. Jälleen kerran! Vaimosi syö vain Heinz-ketsuppia ei Feliz. Vaimosi mököttää seuraavan päivän.",
-         "Maistat lempituotteesi uutuusmakua. Se ei maistu hyvältä eikä huonolta."
+        {
          
         };
 
         readonly List<string> MisfortunesChinese = new()
-        { "半夜玩Metallica的全杀会让你的邻居早上给你带来厄运.",
-            "玩排乐透会导致金钱损失",
-            "由于猫粮用完了，猫会抓你最喜欢的壁纸",
-            "多年来，您一直以 Pasi 称呼您的邻居。今天你会发现帕西的真名是萨米",
-            "你走进客厅里的乐高积木。奇怪的是，你没有孩子，上一次玩或拥有乐高积木是几十年前的事了。你不会在几周内忘记这件事",
-            "你买错了番茄酱。再次！你的妻子只吃亨氏番茄酱，不吃菲利斯。你的妻子第二天会留下来",
-            "您品尝您喜爱的产品的新口味。它尝起来既不好也不坏。"
+        { 
         };
 
         public void GetMisfortune()
